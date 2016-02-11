b2aefc65e3965c6af52b8fa075975466944eb616
pandaronincheese <brittney-mikus@uiowa.edu>
2016-02-11 22:50:58 +0000

    started api book tutorial

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
+  
+  namespace :api , defaults {format: :json} do
+    
+  end
+  
+  get 'welcome' => 'pages#home'
+  
+  get '/students' => 'students#index'
+  
+  get '/students/new' => 'students#new'
+  
+  post 'students' => 'students#create'
+  
+  get '/students/:id' => 'students#show', as: :id
+  
+  get 'student/:id/edit' => 'students#edit', as: :edit_student
+  patch 'studnets/:id' => 'students#update'
+  
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
